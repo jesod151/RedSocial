5eef601df3f3ce039a4648a4a44ae95b47bbf341
jesod151 <32991773+jesod151@users.noreply.github.com>
2019-06-07 20:01:55 -0600

    ejemplo listeners

--- RedSocial/app/src/main/java/com/example/redsocial/FeedFragment.java
+++ RedSocial/app/src/main/java/com/example/redsocial/FeedFragment.java
@@ -7,15 +7,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.redsocial.R;
 
 public class FeedFragment extends Fragment {
 
+    View current;
+    TextView txtPublicacion;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater layoutInflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return layoutInflater.inflate(R.layout.feed_layout, container, false);
+        current = layoutInflater.inflate(R.layout.feed_layout, container, false);
+
+        txtPublicacion = current.findViewById(R.id.txtPublicacion);
+        txtPublicacion.setOnClickListener(new TextView.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(current.getContext(), "Hello world", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return current;
     }
 
 
