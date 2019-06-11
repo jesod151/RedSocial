96a9ed8a31da25e16e0dd6148f9213e7190d2828
jesod151 <32991773+jesod151@users.noreply.github.com>
2019-06-10 18:43:50 -0600

    Busquedas

--- RedSocial/app/src/main/java/com/example/redsocial/SearchFragment.java
+++ RedSocial/app/src/main/java/com/example/redsocial/SearchFragment.java
@@ -1,20 +1,156 @@
 package com.example.redsocial;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.redsocial.DataBaseObjects.Post;
+import com.example.redsocial.DataBaseObjects.User;
+import com.example.redsocial.utils.RecyclerViewAdapterPost;
+import com.example.redsocial.utils.RecyclerViewAdapterUsers;
+import com.example.redsocial.utils.UserPreferences;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static android.app.Activity.RESULT_OK;
 
 public class SearchFragment extends Fragment {
+
+    private Spinner spinner;
+    private View current;
+    private TextView filter;
+    private ImageView search;
+    private RecyclerView recyclerView;
+    private UserPreferences userPreferences;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater layoutInflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return layoutInflater.inflate(R.layout.search_layout, container, false);
+        current = layoutInflater.inflate(R.layout.search_layout, container, false);
+        userPreferences = new UserPreferences(current.getContext());
+
+        filter = current.findViewById(R.id.txtSearchFilter);
+        search = current.findViewById(R.id.imgSearch);
+        recyclerView = current.findViewById(R.id.searchRecyclerView);
+        spinner = current.findViewById(R.id.spinnerSearch);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(current.getContext(), R.array.searchs, R.layout.simple_list_item_1);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        search.setOnClickListener(new ImageView.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!spinner.getSelectedItem().toString().equals("")){
+                    if(spinner.getSelectedItem().toString().equals("Usuarios")){
+                        searchForUsers();
+                    }
+                    else{
+                        searchForPosts();
+                    }
+                }
+            }
+        });
+        return current;
     }
 
+    public void searchForUsers(){
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference collectionReference = db.collection("Users");
+
+        Query query = collectionReference;
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+
+                    ArrayList<User> users = new ArrayList<>();
+
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        if(document.getString("name").toUpperCase().contains(filter.getText().toString().toUpperCase())
+                                || document.getString("email").toUpperCase().contains(filter.getText().toString().toUpperCase())){
+                            users.add(new User(document.getString("name"),
+                                            "",
+                                            document.getString("email"),
+                                            document.getString("photo")));
+                        }
+                    }
+                    setUsers(users);
+                }
+            }
+        });
+
+    }
+
+    public void searchForPosts(){
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference collectionReference = db.collection("Posts");
+
+        Query query = collectionReference;
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+
+                    ArrayList<Post> posts = new ArrayList<>();
+
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        if(document.getString("texto").toUpperCase().contains(filter.getText().toString().toUpperCase())) {
+                            posts.add(0, document.toObject(Post.class));
+                        }
+                    }
+                    setPosts(posts);
+                }
+            }
+        });
+    }
+
+    private void setPosts(ArrayList<Post> posts){
+        recyclerView = current.findViewById(R.id.searchRecyclerView);
+        RecyclerViewAdapterPost recyclerViewAdapterPost = new RecyclerViewAdapterPost(current.getContext(), posts, this);
+        recyclerView.setAdapter(recyclerViewAdapterPost);
+        recyclerView.setLayoutManager(new LinearLayoutManager(current.getContext()));
+    }
+
+    private void setUsers(ArrayList<User> users){
+        recyclerView = current.findViewById(R.id.searchRecyclerView);
+        RecyclerViewAdapterUsers recyclerViewAdapterUsers = new RecyclerViewAdapterUsers(current.getContext(), users, this);
+        recyclerView.setAdapter(recyclerViewAdapterUsers);
+        recyclerView.setLayoutManager(new LinearLayoutManager(current.getContext()));
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1){//posts
+            searchForPosts();
+        }
+        else{//users
+            searchForUsers();
+        }
+    }
 
 
 }
--- RedSocial/app/src/main/java/com/example/redsocial/utils/RecyclerViewAdapterUsers.java
+++ RedSocial/app/src/main/java/com/example/redsocial/utils/RecyclerViewAdapterUsers.java
@@ -3,7 +3,6 @@ package com.example.redsocial.utils;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,10 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.redsocial.DataBaseObjects.Post;
+import com.example.redsocial.DataBaseObjects.User;
 import com.example.redsocial.FeedFragment;
 import com.example.redsocial.Post_detail_layout;
 import com.example.redsocial.R;
@@ -31,32 +30,25 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class RecyclerViewAdapterPost extends RecyclerView.Adapter<RecyclerViewAdapterPost.ViewHolder>{
+public class RecyclerViewAdapterUsers extends RecyclerView.Adapter<RecyclerViewAdapterUsers.ViewHolder>{
 
     private Context context;
-    private ArrayList<Post> posts;
+    private ArrayList<User> users;
     private FeedFragment feedFragment;
     private SearchFragment searchFragment;
     private UserPreferences userPreferences;
 
-    public RecyclerViewAdapterPost(Context context, ArrayList<Post> posts, FeedFragment feedFragment) {
+    public RecyclerViewAdapterUsers(Context context, ArrayList<User> users, SearchFragment searchFragment) {
         this.context = context;
         this.userPreferences = new UserPreferences(context);
-        this.posts = posts;
-        this.feedFragment = feedFragment;
-    }
-
-    public RecyclerViewAdapterPost(Context context, ArrayList<Post> posts, SearchFragment searchFragment) {
-        this.context = context;
-        this.userPreferences = new UserPreferences(context);
-        this.posts = posts;
+        this.users = users;
         this.searchFragment = searchFragment;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.feedpost_layout, viewGroup, false);
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.user_search_layout, viewGroup, false);
         ViewHolder holder = new ViewHolder(view);
         return holder;
     }
@@ -64,31 +56,14 @@ public class RecyclerViewAdapterPost extends RecyclerView.Adapter<RecyclerViewAd
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
 
-        viewHolder.likes.setText(Integer.toString(posts.get(i).getLikes()));
-        viewHolder.dislikes.setText(Integer.toString(posts.get(i).getDislikes()));
-        viewHolder.texto.setText(posts.get(i).getUserID() + "\n" + getDistanciaFecha(posts.get(i).getFechaCreacion()) + "\n" + posts.get(i).getTexto());
-        if(!posts.get(i).getImagenUrl().equals("")){
-            Glide.with(context).load(posts.get(i).getImagenUrl()).into(viewHolder.imagen);
+        viewHolder.texto.setText(users.get(i).getNombre());
+        if(!users.get(i).getImagenUrl().equals("")){
+            Glide.with(context).load(users.get(i).getImagenUrl()).into(viewHolder.imagen);
         }
-        if(userPreferences.getEmail().equals(posts.get(i).getUserID())){
-            viewHolder.btnDelete.setVisibility(View.VISIBLE);
-            viewHolder.btnDelete.setClickable(true);
-            viewHolder.btnDelete.setOnClickListener(new ImageView.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    FirebaseFirestore db = FirebaseFirestore.getInstance();
-                    DocumentReference documentReference = db.collection("Posts").document(posts.get(i).getPostID());
-                    documentReference.delete();
-                    feedFragment.getPosts();
-                }
-            });
-        }
-        setUserImageUrl(posts.get(i), viewHolder.imgUser);
         viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                goPostDetail(posts.get(i));
             }
         });
 
@@ -96,32 +71,25 @@ public class RecyclerViewAdapterPost extends RecyclerView.Adapter<RecyclerViewAd
 
     @Override
     public int getItemCount() {
-        return posts.size();
+        return users.size();
     }
 
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        private TextView texto, likes, dislikes;
-        private ImageView btnLike, btnDislike, btnDelete, imagen, imgUser;
+        private TextView texto;
+        private ImageView imagen;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            texto = itemView.findViewById(R.id.txtPost);
-            likes = itemView.findViewById(R.id.txtLikes);
-            dislikes = itemView.findViewById(R.id.txtDislikes);
-            btnLike = itemView.findViewById(R.id.imgLike);
-            btnDislike = itemView.findViewById(R.id.imgDislike);
-            btnDelete = itemView.findViewById(R.id.imgDeletePost);
-            imagen = itemView.findViewById(R.id.imgPost);
-            imgUser = itemView.findViewById(R.id.imgPostUserImage);
-            btnDelete = itemView.findViewById(R.id.imgDeletePost);
+            texto = itemView.findViewById(R.id.txtUserSearch);
+            imagen = itemView.findViewById(R.id.imgUserSearch);
 
         }
     }
 
-    private void goPostDetail(Post post){
+    /*private void goPostDetail(Post post){
         Intent intent = new Intent(context, Post_detail_layout.class);
         intent.putExtra("userID", post.getUserID());
         intent.putExtra("texto", post.getTexto());
@@ -131,42 +99,14 @@ public class RecyclerViewAdapterPost extends RecyclerView.Adapter<RecyclerViewAd
         intent.putExtra("dislikes", post.getDislikes());
         intent.putExtra("fecha", getDistanciaFecha(post.getFechaCreacion()));
         intent.putExtra("id", post.getPostID());
-
+        
         if(feedFragment != null){
             feedFragment.startActivityForResult(intent, 2);
         }
         else if(searchFragment != null){
             searchFragment.startActivityForResult(intent, 1);
         }
-
-
-    }
-
-    private String getDistanciaFecha(Date date){
-
-        Date actual = new Date();
-        long diff = actual.getTime() - date.getTime();
-        int diffMinutes = (int) Math.floor(diff / (60 * 1000) % 60);
-        int diffHours = (int) Math.floor(diff / (60 * 60 * 1000));
-
-        Log.d("fecja", Integer.toString(diffHours));
-        Log.d("fecja", Integer.toString(diffMinutes));
-
-        if(diffHours >= 24){
-            return "hace " + Double.toString((int) Math.floor(diffHours / 24)) + " dias";
-        }
-        if(diffHours >= 1){
-            return "hace " + Integer.toString(diffHours) + " horas";
-        }
-        if(diffMinutes >= 60){
-            return "hace " + Double.toString((int) Math.floor(diffMinutes / 60)) + " horas";
-        }
-        if(diffMinutes >= 1){
-            return "hace " + Integer.toString(diffMinutes) + " minutos";
-        }
-
-        return "hace menos de un minuto";
-    }
+    }*/
 
     public void setUserImageUrl(Post post, ImageView img){
 
@@ -188,4 +128,5 @@ public class RecyclerViewAdapterPost extends RecyclerView.Adapter<RecyclerViewAd
             }
         });
     }
+
 }
